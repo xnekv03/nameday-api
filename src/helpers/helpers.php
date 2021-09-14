ed5d029332ad54a94581015663020f2f47cc0f5b
VojtaN <vn@abalin.net>
2021-09-14 13:37:33 +0200

    Refactor (#79)
    * style ci
    
    * Apply fixes from StyleCI (#7)
    
    * Fix styling
    
    * Apply fixes from StyleCI (#11)
    
    * Fix styling
    
    * Apply fixes from StyleCI (#19)
    
    * update packages
    
    * Apply fixes from StyleCI (#23)
    
    * Fix styling
    
    * Apply fixes from StyleCI (#43)
    
    * del wf
    
    Co-authored-by: xnekv03 <xnekv03@users.noreply.github.com>

--- src/helpers/helpers.php
+++ src/helpers/helpers.php
@@ -1,6 +1,11 @@
 <?php
 
-function getAllSupportedCountries(): array
-{
-    return json_decode(file_get_contents('src/data/countryList.json'), true);
+if (!function_exists('getAllSupportedCountries')) {
+    /**
+     * @return array|null
+     */
+    function getAllSupportedCountries(): array
+    {
+        return json_decode(file_get_contents('src/data/countryList.json'), true);
+    }
 }
