ad0a7d0b3a0caef0c2a6e048f5253381d74a47c2
VojtaN <vn@abalin.net>
2022-02-25 13:54:15 +0100

    Apply fixes from StyleCI (#82)

--- src/ApiNamedayClass.php
+++ src/ApiNamedayClass.php
@@ -8,104 +8,111 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Exception\InvalidArgumentException;
 use Xnekv03\ApiNameday\traits\ValidationTrait;
 
-class ApiNamedayClass{
-  protected Carbon $carbonToday;
-  protected string $baseUrl = 'https://nameday.abalin.net/api/V1/';
-  protected string $defaultTimeZone = 'Europe/Prague';
-  use ValidationTrait;
+class ApiNamedayClass
+{
+    protected Carbon $carbonToday;
+    protected string $baseUrl = 'https://nameday.abalin.net/api/V1/';
+    protected string $defaultTimeZone = 'Europe/Prague';
+    use ValidationTrait;
 
-  /**
-   * @param string|null $timeZone
-   *
-   * @throws Exception
-   */
-  public function __construct(string $timeZone = null){
-    try{
-      $this->carbonToday = Carbon::now($timeZone ?? $this->defaultTimeZone);
-    }
-    catch(Exception $e){
-      throw new Exception($e->getMessage());
-    }
-  }
-
-  /**
-   * @throws Exception
-   */
-  public function today($countryCode = null): array{
-    if(!is_null($countryCode)){
-      $this->validateCountryCode($countryCode);
+    /**
+     * @param string|null $timeZone
+     *
+     * @throws Exception
+     */
+    public function __construct(string $timeZone = null)
+    {
+        try {
+            $this->carbonToday = Carbon::now($timeZone ?? $this->defaultTimeZone);
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
     }
 
-    return $this->getSpecificDay($this->carbonToday, $countryCode);
-  }
+    /**
+     * @throws Exception
+     */
+    public function today($countryCode = null): array
+    {
+        if (!is_null($countryCode)) {
+            $this->validateCountryCode($countryCode);
+        }
 
-  /**
-   * @throws Exception
-   */
-  public function tomorrow($countryCode = null): array{
-    if(!is_null($countryCode)){
-      $this->validateCountryCode($countryCode);
+        return $this->getSpecificDay($this->carbonToday, $countryCode);
     }
 
-    return $this->getSpecificDay($this->carbonToday->addDay(), $countryCode);
-  }
+    /**
+     * @throws Exception
+     */
+    public function tomorrow($countryCode = null): array
+    {
+        if (!is_null($countryCode)) {
+            $this->validateCountryCode($countryCode);
+        }
 
-  /**
-   * @throws Exception
-   */
-  public function yesterday($countryCode = null): array{
-    if(!is_null($countryCode)){
-      $this->validateCountryCode($countryCode);
+        return $this->getSpecificDay($this->carbonToday->addDay(), $countryCode);
     }
 
-    return $this->getSpecificDay($this->carbonToday->subDay(), $countryCode);
-  }
+    /**
+     * @throws Exception
+     */
+    public function yesterday($countryCode = null): array
+    {
+        if (!is_null($countryCode)) {
+            $this->validateCountryCode($countryCode);
+        }
 
-  /**
-   * @throws \JsonException
-   */
-  public function searchByName(string $name, string $countryCode): array{
-    $this->validateSearchName($name, $countryCode);
-
-    return $this->callApi('getname', [
-      'name' => $name,
-      'country' => $countryCode,
-    ]);
-  }
-
-  private function getSpecificDay(Carbon $date, string $countryCode = null): array{
-    $parameters = [
-      'day' => $date->day,
-      'month' => $date->month,
-    ];
-
-    if(!is_null($countryCode)){
-      $parameters = array_merge($parameters, ['country' => $countryCode]);
+        return $this->getSpecificDay($this->carbonToday->subDay(), $countryCode);
     }
 
-    return $this->callApi('getdate', $parameters);
-  }
+    /**
+     * @throws \JsonException
+     */
+    public function searchByName(string $name, string $countryCode): array
+    {
+        $this->validateSearchName($name, $countryCode);
 
-  /**
-   * @param string $urlParameter
-   *
-   * @return array
-   * @throws \JsonException
-   */
-  private function callApi(string $urlParameter, array $formParams = null): array{
-    try{
-      $response = (new Client())->request('POST', $this->baseUrl.$urlParameter, [
-        'form_params' => $formParams ?? [],
-      ]);
-    }
-    catch(Exception $e){
-      throw new InvalidArgumentException($e->getMessage());
+        return $this->callApi('getname', [
+            'name'    => $name,
+            'country' => $countryCode,
+        ]);
     }
 
-    if($response->getStatusCode() !== 200){
-      throw new \RuntimeException('Unsuccessfull call');
+    private function getSpecificDay(Carbon $date, string $countryCode = null): array
+    {
+        $parameters = [
+            'day'   => $date->day,
+            'month' => $date->month,
+        ];
+
+        if (!is_null($countryCode)) {
+            $parameters = array_merge($parameters, ['country' => $countryCode]);
+        }
+
+        return $this->callApi('getdate', $parameters);
     }
 
-    return json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
-  }
+    /**
+     * @param string $urlParameter
+     *
+     * @throws \JsonException
+     *
+     * @return array
+     */
+    private function callApi(string $urlParameter, array $formParams = null): array
+    {
+        try {
+            $response = (new Client())->request('POST', $this->baseUrl.$urlParameter, [
+                'form_params' => $formParams ?? [],
+            ]);
+        } catch (Exception $e) {
+            throw new InvalidArgumentException($e->getMessage());
+        }
+
+        if ($response->getStatusCode() !== 200) {
+            throw new \RuntimeException('Unsuccessfull call');
+        }
+
+        return json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
+    }
 }
--- src/traits/ValidationTrait.php
+++ src/traits/ValidationTrait.php
@@ -2,52 +2,55 @@
 
 namespace Xnekv03\ApiNameday\traits;
 
-trait ValidationTrait{
-  protected array $supportedCountries = [
-    "at",
-    "cz",
-    "fi",
-    "gr",
-    "lv",
-    "ru",
-    "se",
-    "bg",
-    "dk",
-    "fr",
-    "hu",
-    "lt",
-    "sk",
-    "us",
-    "hr",
-    "ee",
-    "de",
-    "it",
-    "pl",
-    "es",
-  ];
+trait ValidationTrait
+{
+    protected array $supportedCountries = [
+        'at',
+        'cz',
+        'fi',
+        'gr',
+        'lv',
+        'ru',
+        'se',
+        'bg',
+        'dk',
+        'fr',
+        'hu',
+        'lt',
+        'sk',
+        'us',
+        'hr',
+        'ee',
+        'de',
+        'it',
+        'pl',
+        'es',
+    ];
 
-  /**
-   * @throws \Exception
-   */
-  public function validateSearchName(string $name, string $countryCode): void{
-    $min = 3;
-    $max = 20;
-    if(strlen(trim($name)) < $min){
-      throw new \RuntimeException('Name must contain more than '.$min.' characters');
+    /**
+     * @throws \Exception
+     */
+    public function validateSearchName(string $name, string $countryCode): void
+    {
+        $min = 3;
+        $max = 20;
+        if (strlen(trim($name)) < $min) {
+            throw new \RuntimeException('Name must contain more than '.$min.' characters');
+        }
+
+        if (strlen(trim($name)) > $max) {
+            throw new \RuntimeException('Name must contain less then '.$max.' characters');
+        }
+        $this->validateCountryCode($countryCode);
     }
 
-    if(strlen(trim($name)) > $max){
-      throw new \RuntimeException('Name must contain less then '.$max.' characters');
+    /**
+     * @throws \Exception
+     */
+    public function validateCountryCode(string $countryCode)
+    {
+        if (!in_array($countryCode, $this->supportedCountries, true)) {
+            throw new \RuntimeException('Unsupported country');
+        }
     }
-    $this->validateCountryCode($countryCode);
-  }
-
-  /**
-   * @throws \Exception
-   */
-  public function validateCountryCode(string $countryCode){
-    if(!in_array($countryCode, $this->supportedCountries, true)){
-      throw new \RuntimeException('Unsupported country');
-    }
-  }
 }
